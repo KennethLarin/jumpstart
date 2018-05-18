cc0952ea1c8f6456bf3db42476fde90c30f627e3
Effendy <effendylarin@outlook.com>
2018-05-18 14:09:43 -0600

    added navbar effect

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,5 +8,17 @@ $(document).ready(function(){
     pauseOnHover: false
   });
 
-  let userScroll = 0;
+  var lastScrollTop = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - lastScrollTop > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      lastScrollTop = scrollTop;
+    } else if (lastScrollTop - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      lastScrollTop = scrollTop;
+    }
+  }
+  );
 });
