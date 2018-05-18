d8e5533b5ee18934470a41bb6cb0da93bb1e9a8c
Effendy <effendylarin@outlook.com>
2018-05-18 11:14:31 -0600

    Created caroursel effect

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    accesibility: false,
+    autoplay: true,
+    Speed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  let userScroll = 0;
+});
